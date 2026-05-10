ea2410f2bbad849a8fbf6cadf27a2d1c145f784a
lemical07 <icaleladio@gmail.com>
2026-05-10 15:37:22 -0600

    feat: :sparkles: JS
    We bring all the selectors and declare global variables.

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// --- Selectores del DOM ---
+const input = document.getElementById('input-search');
+const btnSearch = document.getElementById('btn-searchh');
+const imageContainer = document.getElementById('img-container');
+const detailsContainer = document.getElementById('details-container');
+const loader = document.getElementById('loader');
+
+// variable principal donde guardaremos los lanzamientos
+let launchList = [];
